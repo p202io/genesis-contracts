e4fdfb7296f2da1adbc8e5e6c67e900e4877a607
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-05-06 23:55:17 +0300

    update

--- validators.js
+++ validators.js
@@ -1,24 +1,24 @@
 const validators = [
   {
-    address: "0x6c468CF8c9879006E22EC4029696E005C2319C9D",
+    address: "0x5D52374EaaeD324A06834f04A4F865c74c6EA260",
     stake: 40, // without 10^18
     balance: 1000 // without 10^18
   },
-  // {
-  //   address: "0x48aA8D4AF32551892FCF08Ad63Be7dD206D46F65",
-  //   stake: 30,
-  //   balance: 0
-  // },
-  // {
-  //   address: "0x61083121D4b6ae002aF0CAD52359ae163e183Ccc",
-  //   stake: 20,
-  //   balance: 0
-  // },
-  // {
-  //   address: "0xaFA4EE2EB4707e51Be14dcf182a03e0C9302BA2C",
-  //   stake: 10,
-  //   balance: 0
-  // }
+  {
+    address: "0x02C82884f93a8b833F88C55eF84B977BB970C88e",
+    stake: 30, // without 10^18
+    balance: 1000 // without 10^18
+  },
+  {
+    address: "0x929E0449635B06cE1AED6C664D1c1cfc029bFe5b",
+    stake: 20, // without 10^18
+    balance: 1000 // without 10^18
+  },
+  {
+    address: "0xf872f2e67D8BF5D902E89f3028DF42047A2944a1",
+    stake: 10, // without 10^18
+    balance: 1000 // without 10^18
+  },
 ]
 
 exports = module.exports = validators
